94358c2d0f14cdb537a88ac7beefd90b703b14df
bhp <bhp@easv.dk>
2020-10-22 13:40:57 +0200

    Done all CRUD for StudentService + StudentServiceTest

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -4,6 +4,8 @@ using Castle.Core.Internal;
 using Interfaces;
 using Model;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Services
 {
@@ -36,7 +38,43 @@ namespace Services
                 && ! s.Address.IsNullOrEmpty()
                 && s.ZipCode > 0
                 && ! s.PostalDistrict.IsNullOrEmpty()
-                && s.Email.Length > 0);
+                && (s.Email == null || s.Email != ""));
+        }
+
+        public void UpdateStudent(Student s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentException("Student is missing");
+            }
+            if (! IsValidStudent(s))
+            {
+                throw new ArgumentException("Invalid student property");
+            }
+            if (repo.GetById(s.Id) == null)
+            {
+                throw new InvalidOperationException("Student does not exist");
+            }
+            repo.Update(s);
+        }
+
+        public void RemoveStudent(Student s)
+        {
+            if (repo.GetById(s.Id) == null)
+            {
+                throw new InvalidOperationException("Cannot remove not-existing student");
+            }
+            repo.Remove(s);
+        }
+
+        public Student GetStudentById(int id)
+        {
+            return repo.GetById(id);
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return repo.GetAll();
         }
     }
 }
