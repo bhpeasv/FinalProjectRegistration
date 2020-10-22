6c28c0ec006843b26d61e3273bfb897fc949eae5
bhp <bhp@easv.dk>
2020-10-22 11:20:47 +0200

    Added AddStudent in StudentService + test

--- Services/StudentService.cs
+++ Services/StudentService.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿
+
+using Interfaces;
+using Model;
+using System;
 
 namespace Services
 {
     public class StudentService
     {
+        private IStudentRepository repo;
+
+        public StudentService(IStudentRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public void AddStudent(Student s)
+        {
+            repo.Add(s);
+        }
     }
 }
--- XUnitTestProject/StudentServiceTest.cs
+++ XUnitTestProject/StudentServiceTest.cs
@@ -1,3 +1,7 @@
+using Interfaces;
+using Model;
+using Moq;
+using Services;
 using System;
 using Xunit;
 
@@ -5,10 +9,43 @@ namespace XUnitTestProject
 {
     public class StudentServiceTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private Mock<IStudentRepository> repoMock;
 
+        public StudentServiceTest()
+        {
+            repoMock = new Mock<IStudentRepository>();
+        }
+
+        [Fact]
+        public void CreateStudentService()
+        {
+            // arrange
+            IStudentRepository repo = repoMock.Object;
+
+            // act
+            StudentService service = new StudentService(repo);
+
+            // assert
+            Assert.NotNull(service);
+        }
+
+        [Fact]
+        public void AddValidStudent()
+        {
+            // arrange
+            IStudentRepository repo = repoMock.Object;
+            StudentService service = new StudentService(repo);
+
+            Student s = new Student()
+            {
+                Id = 1
+            };
+
+            // act
+            service.AddStudent(s);
+
+            // assert
+            repoMock.Verify(repo => repo.Add(It.Is<Student>((st => st == s))), Times.Once);
         }
     }
 }
